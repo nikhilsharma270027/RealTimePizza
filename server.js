f9ffe9695b6d4bb98e22526ecf83bfb4497c851a
nikhil <nikhilsharma270027@gmail.com>
2023-10-09 17:45:12 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -8,15 +8,27 @@ const PORT = process.env.PORT || 3300
 
 // Asset
 app.use(express.static('public'));
-app.get('/', (req,res) => {
-    res.render('home');
-});
 
 //set Template Engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
+                                    //part5 15:00
+app.get('/', (req,res) => {
+    res.render('home');
+});
 
+app.get('/cart', (req,res) => {
+    res.render('customers/cart');
+})
+
+app.get('/login', (req,res) => {
+    res.render('auth/login');
+})
+
+app.get('/register', (req,res) => {
+    res.render('auth/register');
+})
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
